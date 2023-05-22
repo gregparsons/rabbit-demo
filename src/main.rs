919cd5096ee34e509af2b3de1f8c4707751f5b38
gp <greg.parsons@inclimesolutions.com>
2023-05-21 21:27:00 -0700

    basic improvements on lapin sample

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //! main.rs
 
+use std::thread;
+use std::time::Duration;
 use futures_lite::stream::StreamExt;
 use lapin::{
     options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
@@ -7,7 +9,12 @@ use lapin::{
 };
 use tracing::info;
 
+const CHANNEL_A:&'static str = "channel_a";
+
 fn main() -> Result<()> {
+
+
+
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info");
     }
@@ -27,31 +34,32 @@ fn main() -> Result<()> {
         let channel_b = conn.create_channel().await?;
 
         // establish queue named hello
-        let queue = channel_a.queue_declare("hello", QueueDeclareOptions::default(), FieldTable::default()).await?;
+        let queue = channel_a.queue_declare(CHANNEL_A, QueueDeclareOptions::default(), FieldTable::default()).await?;
         info!(?queue, "Declared queue");
 
         let mut consumer = channel_b
-            .basic_consume("hello", "my_consumer", BasicConsumeOptions::default(), FieldTable::default())
+            .basic_consume(CHANNEL_A, "my_consumer", BasicConsumeOptions::default(), FieldTable::default())
             .await?;
 
         // consumer thread
         async_global_executor::spawn(async move {
-            info!("will consume");
-            while let Some(delivery) = consumer.next().await {
-                let delivery = delivery.expect("error in consumer");
-                delivery.ack(BasicAckOptions::default()).await.expect("ack");
+            info!("consume");
+            while let Some(message_result) = consumer.next().await {
+                let message = message_result.expect("error in consumer");
+                info!("[consumer] message: {:?}", &message);
+                message.ack(BasicAckOptions::default()).await.expect("ack");
             }
         }).detach();
-
         info!("spawned consumer; spawning publisher");
 
         // publish a message
         let payload = b"Hello world!";
         loop {
-            let confirm = channel_a.basic_publish("", "hello", BasicPublishOptions::default(), payload, BasicProperties::default()).await?.await?;
+            let confirmation = channel_a.basic_publish("", CHANNEL_A, BasicPublishOptions::default(), payload, BasicProperties::default()).await?.await?;
             // something about this block in the lapin example is lazy; adding this info! gets it to run
-            info!("[publish] confirmation: {:?}", &confirm);
-            assert_eq!(confirm, Confirmation::NotRequested);
+            info!("[publisher] confirmation: {:?}", &confirmation);
+            assert_eq!(confirmation, Confirmation::NotRequested);
+            thread::sleep(Duration::from_secs(3));
         }
 
     })
